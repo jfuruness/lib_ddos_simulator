e8409a1145de642658125fb8bd34b0bd133ec72d
jfuruness <jfuruness@gmail.com>
2020-10-03 18:34:45 -0400

    changed defaults on animations

--- lib_ddos_simulator/__main__.py
+++ lib_ddos_simulator/__main__.py
@@ -24,11 +24,11 @@ def main():
     parser = ArgumentParser(description="Runs a DDOS simulation")
     # NOTE: these defaults are chosen that way because they work for the animator
     # Changing these defaults will result in worse animations
-    parser.add_argument("--num_users", type=int, dest="num_users", default=96)
-    parser.add_argument("--num_attackers", type=int, dest="num_attackers", default=24)
-    parser.add_argument("--num_buckets", type=int, dest="num_buckets", default=16)
+    parser.add_argument("--num_users", type=int, dest="num_users", default=192)
+    parser.add_argument("--num_attackers", type=int, dest="num_attackers", default=48)
+    parser.add_argument("--num_buckets", type=int, dest="num_buckets", default=32)
     parser.add_argument("--threshold", type=int, dest="threshold", default=10)
-    parser.add_argument("--rounds", type=int, dest="rounds", default=20)
+    parser.add_argument("--rounds", type=int, dest="rounds", default=40)
     parser.add_argument("--debug", dest="debug", default=False, action='store_true')
     parser.add_argument("--animate", dest="animate", default=False, action='store_true')
     parser.add_argument("--graph_combos", dest="graph_combos", default=False, action='store_true')
