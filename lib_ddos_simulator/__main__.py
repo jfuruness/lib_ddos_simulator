863b81b87fe1e39bc74be300d406fa43ba6e334c
jfuruness <jfuruness@gmail.com>
2020-10-03 02:23:00 -0400

    animations set to run

--- lib_ddos_simulator/__main__.py
+++ lib_ddos_simulator/__main__.py
@@ -52,9 +52,8 @@ def main():
                        args.num_attackers,  # number of attackers
                        args.num_buckets,  # number of buckets
                        args.threshold,  # Threshold
-                       #[Protag_Manager,
-                        #[Bounded_Manager],
-                        [] + Sieve_Manager_Base.runnable_managers,
+                       [Protag_Manager,
+                        Bounded_Manager] + Sieve_Manager_Base.runnable_managers,
                        graph_dir=args.graph_dir,
                        save=args.save,
                        high_res=args.high_res).run(args.rounds, animate=True)
