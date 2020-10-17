50b34b819d410607c9cd487ddd5fcfc879fca354
jfuruness <jfuruness@gmail.com>
2020-10-17 18:55:14 -0400

    trying to fix manifest.in problem

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ with open("README.md", "r") as fh:
 setup(
     name='lib_ddos_simulator',
     packages=find_packages(),
-    version='0.0.5',
+    include_package_data=True,
+    version='0.0.5b',
     author='Justin Furuness and Anna Gorbenko',
     author_email='jfuruness@gmail.com, agorbenko97@gmail.com',
     url='https://github.com/jfuruness/lib_ddos_simulator.git',
