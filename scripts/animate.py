9bede495880489b5efe9109eb56bd73ebc6905b3
Justin Furuness <jfuruness@gmail.com>
2023-06-28 04:03:18 -0400

    updates to animations

--- scripts/animate.py
+++ scripts/animate.py
@@ -1,16 +1,20 @@
 from pathlib import Path
 
 from lib_ddos_simulator import DDOS_Simulator, Basic_Attacker
-from lib_ddos_simulator import Motag_Manager_20_Bucket
+from lib_ddos_simulator import Motag_Manager_40_Bucket
+from lib_ddos_simulator import Protag_Manager_Merge
 
 # Parameters to change for the animations
 ############################################################
-num_users = 40
-num_attackers = 4
+num_users = 100
+num_attackers = int(num_users * .1)
 num_buckets = 1
 num_rounds = 10
 # All the managers to run. See manager section for a list
-manager_child_classes = [Motag_Manager_20_Bucket]
+manager_child_classes = [
+    # Protag_Manager_Merge,
+    Motag_Manager_40_Bucket
+]
 # The type of attacker. See attacker section for a list
 attacker_cls = Basic_Attacker
 ############################################################
